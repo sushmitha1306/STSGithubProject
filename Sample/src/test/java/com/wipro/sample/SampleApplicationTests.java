f657002975aabc6e21fb08254fffbed345490e3f
7HP_SP1 <7HP_SP1@PC2016110702IRN>
2020-04-07 17:57:19 +0530

    final commit

--- Sample/src/test/java/com/wipro/sample/SampleApplicationTests.java
+++ Sample/src/test/java/com/wipro/sample/SampleApplicationTests.java
@@ -1,13 +1,8 @@
-package com.wipro.Sample;
+package com.wipro.sample;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SampleApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
